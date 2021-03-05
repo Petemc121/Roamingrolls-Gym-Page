f37c16110e4dbc1eb604c8dc3027131d803a4ded
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-03-05 10:23:01 +0900

    Create InstructorDeleteAjax.js

---
+++ InstructorDeleteAjax.js
@@ -0,0 +1,60 @@
+
+
+window.onload = function() {
+
+    const minusInstructor = document.getElementsByClassName('minusInstructor');
+   
+    for(i=0; i<minusInstructor.length; i++) {
+
+    minusInstructor[i].on('click',function(){
+        var deleteButton = this.id
+            $.ajax({
+                type:'POST',
+                url: ajax_object.ajax_url + '?action=delete_instructors',
+                data: 'minus_id=' + deleteButton,
+                success:function() {
+                    alert('success!');
+                }
+            }); 
+        });
+
+    }
+}
+
+        // $('#minusInstructor1').on('click',function(){
+        //     $.ajax({
+        //         type:'POST',
+        //         url: ajax_object.ajax_url + '?action=delete_instructors',
+        //         data:'minusInstructor2'
+
+        //     }); 
+        // });
+
+        // $('#minusInstructor2').on('click',function(){
+        //     $.ajax({
+        //         type:'POST',
+        //         url: ajax_object.ajax_url + '?action=delete_instructors',
+        //         data:'minusInstructor3'
+
+        //     }); 
+        // });
+
+        // $('#minusInstructor3').on('click',function(){
+        //     $.ajax({
+        //         type:'POST',
+        //         url: ajax_object.ajax_url + '?action=delete_instructors',
+        //         data:'minusInstructor4'
+
+        //     }); 
+        // });
+
+        // $('#minusInstructor4').on('click',function(){
+        //     $.ajax({
+        //         type:'POST',
+        //         url: ajax_object.ajax_url + '?action=delete_instructors',
+        //         data:'minusInstructor5'
+        //     }); 
+        // });
+
+   
+
